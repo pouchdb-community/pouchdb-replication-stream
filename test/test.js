b52a192131ba58b2043bbc42604ab3f98411f474
Nolan Lawson <nolan.lawson@gmail.com>
2014-10-31 19:57:24 -0400

    (#10) - add seq checkpoints

--- test/test.js
+++ test/test.js
@@ -32,7 +32,6 @@ if (process.browser) {
 } else {
   dbs = process.env.TEST_DB;
 }
-var origN = 180;
 dbs.split(',').forEach(function (db) {
   var dbType = /^http/.test(db) ? 'http' : 'local';
   tests(db, dbType);
@@ -42,8 +41,23 @@ function tests(dbName, dbType) {
 
   var db;
   var remote;
+  var out;
+  var stream;
+  var outStream;
 
   beforeEach(function () {
+    this.timeout(30000);
+    out = [];
+    stream = through(function (chunk, _, next) {
+      out.push(chunk);
+      next();
+    });
+    outStream = noms(function (next) {
+      out.forEach(function (item) {
+        this.push(item);
+      }, this);
+      next(null, null);
+    });
     db = new Pouch(dbName);
     return db.then(function () {
       remote = new Pouch(dbName + '_remote');
@@ -52,26 +66,17 @@ function tests(dbName, dbType) {
   });
 
   afterEach(function () {
+    this.timeout(30000);
     return db.destroy().then(function () {
       return remote.destroy();
     });
   });
+
   describe(dbType + ': hello test suite', function () {
-    origN += 20;
-    var n = origN;
-    var out = [];
-   
-    var stream = through(function (chunk, _, next) {
-      out.push(chunk);
-      next();
-    });
-    var outStream = noms(function (next) {
-      out.forEach(function (item) {
-        this.push(item);
-      }, this);
-      next(null, null);
-    });
-    it('should dump a basic db', function (done) {
+    this.timeout(30000);
+
+    it('should dump and load a basic db', function (done) {
+      var n = 180;
       random(n).pipe(db.createWriteStream()).on('finish', function () {
         db.dump(stream).then(function () {
           out.map(function (item, i) {
@@ -79,21 +84,59 @@ function tests(dbName, dbType) {
               return 1;
             }
             var out = JSON.parse(item);
-            return out.docs.length;
+            return out.docs ? out.docs.length : 0;
           }).reduce(function (a, b) {
             return a + b;
           }).should.equal(n + 1, n + ' docs dumped plus meta data');
+        }).then(function () {
+          return remote.load(outStream);
+        }).then(function () {
+          return remote.allDocs();
+        }).then(function (res) {
+          res.rows.should.have.length(n, n + ' docs replicated');
           done();
         }).catch(done);
       });
     });
-    it('should load a basic db', function (done) {
-      return remote.load(outStream).then(function () {
-        return remote.allDocs();
-      }).then(function (res) {
-        res.rows.should.have.length(n, n + ' docs replicated');
-        done();
-      }).catch(done);
+
+    it('should dump and load an empty db', function (done) {
+      var n = 0;
+      random(n).pipe(db.createWriteStream()).on('finish', function () {
+        db.dump(stream).then(function () {
+          out.map(function (item, i) {
+            if (!i) {
+              return 1;
+            }
+            var out = JSON.parse(item);
+            return out.docs ? out.docs.length : 0;
+          }).reduce(function (a, b) {
+            return a + b;
+          }).should.equal(n + 1, n + ' docs dumped plus meta data');
+        }).then(function () {
+          return remote.load(outStream);
+        }).then(function () {
+          return remote.allDocs();
+        }).then(function (res) {
+          res.rows.should.have.length(n, n + ' docs replicated');
+          done();
+        }).catch(done);
+      });
+    });
+
+    it('should dump with seqs', function (done) {
+      var lastSeq;
+      random(180).pipe(db.createWriteStream()).on('finish', function () {
+        db.dump(stream).then(function () {
+          out.forEach(function (item) {
+            item = JSON.parse(item);
+            if (item.seq) {
+              lastSeq = item.seq;
+            }
+          });
+          lastSeq.should.equal(180);
+          done();
+        }).catch(done);
+      });
     });
   });
 

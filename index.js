dc907dce6749beb3447c24351d320f33fe13e1ed
Conor Mac Aoidh <conor@macaoidh.ie>
2015-02-16 15:17:02 +0000

    fix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ exports.plugin.dump = utils.toPromise(function (writableStream, opts, callback) 
       db_info: info
     };
     writableStream.write(JSON.stringify(header) + '\n');
-    if(!opts.batch_size){
+    if(!!!opts.batch_size){
       opts.batch_size = DEFAULT_BATCH_SIZE;
     }
     return self.replicate.to(output, opts);

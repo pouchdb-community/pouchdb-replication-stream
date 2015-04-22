13dd36301cf70db847cfe41e696254dde8668a69
Nolan Lawson <nolan.lawson@gmail.com>
2015-04-22 14:46:17 -0400

    fix coverage

--- to-buffer-stream.js
+++ to-buffer-stream.js
@@ -4,6 +4,8 @@ var through = require('through2').obj;
 
 module.exports = function () {
   return through(function (chunk, _, next) {
+    // this only applies in the browser
+    /* istanbul ignore next */
     if (!(chunk instanceof Buffer) && Buffer.isBuffer(chunk)) {
       chunk = new Buffer(chunk);
     }
